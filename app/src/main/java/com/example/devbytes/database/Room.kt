9e957f416112187f47976d834228e38a7627fcec
luispolippo <luispolippo@hotmail.com>
2021-04-29 16:24:11 -0300

    implemented the database in Room.kt

--- app/src/main/java/com/example/devbytes/database/Room.kt
+++ app/src/main/java/com/example/devbytes/database/Room.kt
@@ -1,10 +1,10 @@
 package com.example.devbytes.database
 
+import android.content.Context
 import androidx.lifecycle.LiveData
-import androidx.room.Dao
-import androidx.room.Insert
-import androidx.room.OnConflictStrategy
-import androidx.room.Query
+import androidx.room.*
+
+private lateinit var INSTANCE: VideosDatabase
 
 @Dao
 interface VideoDao{
@@ -16,3 +16,20 @@ interface VideoDao{
     fun insertAll(vararg videos: DatabaseVideo )
 }
 
+@Database(entities = [DatabaseVideo::class], version = 1)
+abstract class VideosDatabase: RoomDatabase(){
+    abstract val videoDao: VideoDao
+}
+
+fun getDatabase(context: Context): VideosDatabase{
+    synchronized(VideosDatabase::class.java) {
+        if (!::INSTANCE.isInitialized) {
+            INSTANCE = Room.databaseBuilder(
+                context.applicationContext,
+                VideosDatabase::class.java,
+                "videos"
+            ).build()
+        }
+    }
+    return INSTANCE
+}
