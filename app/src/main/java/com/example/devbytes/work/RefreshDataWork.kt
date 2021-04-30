018d0c6c00bd2325a693a5e82203e19b5e28a6a4
luispolippo <luispolippo@hotmail.com>
2021-04-30 16:17:04 -0300

    implemented work library for background processing

--- app/src/main/java/com/example/devbytes/work/RefreshDataWork.kt
+++ app/src/main/java/com/example/devbytes/work/RefreshDataWork.kt
@@ -1,2 +1,25 @@
 package com.example.devbytes.work
 
+import android.content.Context
+import androidx.work.CoroutineWorker
+import androidx.work.WorkerParameters
+import com.example.devbytes.database.getDatabase
+import com.example.devbytes.repository.VideosRepository
+import retrofit2.HttpException
+
+class RefreshDataWork(appContext: Context, params: WorkerParameters): CoroutineWorker(appContext, params){
+
+    override suspend fun doWork(): Result {
+        val database = getDatabase(applicationContext)
+        val repository = VideosRepository(database)
+
+        return try {
+            repository.refreshVideos()
+            Result.success()
+        } catch (e: HttpException){
+            Result.retry()
+        }
+    }
+
+
+}
