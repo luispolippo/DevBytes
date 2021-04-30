1cc026ca119e76e400d80732bd5925b82ce96a72
luispolippo <luispolippo@hotmail.com>
2021-04-30 15:50:12 -0300

    update the DevByteViewModel.kt to use repository

--- app/src/main/java/com/example/devbytes/viewmodels/DevByteViewModel.kt
+++ app/src/main/java/com/example/devbytes/viewmodels/DevByteViewModel.kt
@@ -2,31 +2,28 @@ package com.example.devbytes.viewmodels
 
 import android.app.Application
 import androidx.lifecycle.*
+import com.example.devbytes.database.getDatabase
 import com.example.devbytes.domain.Video
 import com.example.devbytes.network.Network
 import com.example.devbytes.network.asDomainModel
+import com.example.devbytes.repository.VideosRepository
 import kotlinx.coroutines.launch
 import java.io.IOException
 import java.lang.IllegalArgumentException
 
 class DevByteViewModel(application: Application): AndroidViewModel(application) {
 
-    private val _playlist = MutableLiveData<List<Video>>()
-    val playlist: LiveData<List<Video>>
-        get() = _playlist
+   private val database = getDatabase(application)
+
+    private val videosRepository = VideosRepository(database)
 
     init {
-        refreshDataFromNetwork()
-    }
-
-    private fun refreshDataFromNetwork() = viewModelScope.launch {
-        try {
-            val playlist = Network.devbytes.getPlaylist().await()
-            _playlist.postValue(playlist.asDomainModel())
-        } catch (networkError: IOException){
-
+        viewModelScope.launch {
+            videosRepository.refreshVideos()
         }
     }
+
+    val playlist = videosRepository.videos
 
 
     class Factory(val app: Application):ViewModelProvider.Factory{
